d718f883b019ec51ebeca6b7b126a4b347fe07e5
WumpusThe19th <70040747+WumpusThe19th@users.noreply.github.com>
2024-02-15 13:23:07 -0500

    Create OriginalLab4.java

---
+++ OriginalLab4.java
@@ -0,0 +1,32 @@
+public class OriginalLab4{
+        void countDown(int num){
+            if (num == 0) // test
+                System.out.println("Blastoff!");
+            else{
+                System.out.println("...");
+                countDown(num-1); // recursive call
+            } 
+        }
+        
+        int gcd(int x, int y) {
+            if (x % y == 0) //base case
+                return y;
+            else
+                return gcd(y, x % y);
+        }
+        
+        int fib(int n){
+            if (n <= 0) // base case
+                return 0;
+            else if (n == 1) // base case
+                return 1;
+            else
+                return fib(n – 1) + fib(n – 2);
+        }
+
+        public static void main(String[] args){
+            countDown(4);
+            gcd(4, 2);
+            fib(5);
+        }
+}
