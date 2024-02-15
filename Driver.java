435ccac8a2d8e7224a49f305ae5c02c619484a3a
WumpusThe19th <70040747+WumpusThe19th@users.noreply.github.com>
2024-02-15 13:27:04 -0500

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,8 @@
+public class Driver{
+        public static void main(String[] args){
+            OriginalLab4 original = new OriginalLab4();
+            original.countDown(4);
+            original.gcd(4, 2);
+            original.fib(5);
+        }
+}
